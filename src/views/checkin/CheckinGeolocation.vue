2e9f6c01a2839821cbeed75871d9f7aec9720da6
Ihor Bohdanov <ihor.bohdanov@right.link>
2024-04-25 14:08:07 +0300

    Implement google map to checkin checkout flow

--- src/views/checkin/CheckinGeolocation.vue
+++ src/views/checkin/CheckinGeolocation.vue
@@ -1,8 +1,14 @@
 <script setup>
-import { IonContent, IonPage, useIonRouter } from "@ionic/vue";
+import {
+  IonContent,
+  IonPage,
+  onIonViewDidEnter,
+  onIonViewWillLeave,
+  useIonRouter,
+} from "@ionic/vue";
 import { Geolocation } from "@capacitor/geolocation";
 import Header from "@/components/Header.vue";
-import { onMounted, ref } from "vue";
+import { ref } from "vue";
 import { GoogleMap } from "@capacitor/google-maps";
 
 const router = useIonRouter();
@@ -15,17 +21,21 @@ const coordinates = ref("test");
 
 const printCurrentPosition = async () => {
   coordinates.value = await Geolocation.getCurrentPosition();
-  console.log("Current position:", coordinates.value);
 };
 
 const apiKey = "TOKEN";
+let googleMap;
 
-onMounted(async () => {
+onIonViewDidEnter(async () => {
   await printCurrentPosition();
 
   const mapRef = document.getElementById("map");
 
-  const newMap = await GoogleMap.create({
+  const body = document.querySelector("body.dark");
+
+  body.classList.toggle("map-transparent");
+
+  googleMap = await GoogleMap.create({
     id: "my-map", // Unique identifier for this map instance
     element: mapRef, // reference to the capacitor-google-map element
     apiKey: apiKey, // Your Google Maps API Key
@@ -35,36 +45,69 @@ onMounted(async () => {
         lat: coordinates.value?.coords?.latitude,
         lng: coordinates.value?.coords?.longitude,
       },
+      panControl: false,
       zoom: 18, // The initial zoom level to be rendered by the map
     },
   });
 
-  const markerId = await newMap.addMarker({
+  const markerId = await googleMap.addMarker({
     coordinate: {
       lat: coordinates.value?.coords?.latitude,
       lng: coordinates.value?.coords?.longitude,
     },
   });
+});
 
-  console.log("newMap", newMap);
+onIonViewWillLeave(() => {
+  const body = document.querySelector("body.dark");
+
+  body.classList.toggle("map-transparent");
 });
 </script>
 
 <template>
   <ion-page>
-    <ion-content class="checkin-page">
-      <Header with-back-button @goBack="prevStep">
-        <slot name="title">
-          {{ $t("user.checkin.checkin") }}
-        </slot>
-      </Header>
-      Test Map
+    <ion-content class="geolocation-page">
+      <div class="ion-padding-horizontal geolocation-header">
+        <Header with-back-button @goBack="prevStep">
+          <slot name="title">
+            {{ $t("user.checkin.checkin") }}
+          </slot>
+        </Header>
+      </div>
+      <div style="height: calc(100% - 70px); width: 100%" id="map"></div>
+      <div class="location-wrapper">
+        <ion-row class="ion-align-items-center ion-justify-content-between">
+          <div>
+            <p>Checkin location</p>
+            <p>Head Office @ Burj Hamoud</p>
+          </div>
+          <ion-button shape="round" color="success">Checkin</ion-button>
+        </ion-row>
+      </div>
     </ion-content>
-    <div style="height: calc(100% - 70px); width: 100%" id="map"></div>
   </ion-page>
 </template>
 
-<style lang="scss">
-.checkin-page {
+<style lang="scss" scoped>
+.geolocation-page {
+  --padding-top: 0;
+  --padding-bottom: 0;
+  --padding-start: 0;
+  --padding-end: 0;
+}
+
+.geolocation-header {
+  background: #191c1d;
+}
+
+.location-wrapper {
+  border-radius: 16px 16px 0 0;
+  padding: 24px 16px 8px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #191c1d;
 }
 </style>
