1509f3bea601ae7588e2915c09a0611c8d43dca0
Ihor Bohdanov <ihor.bohdanov@right.link>
2024-04-29 10:53:08 +0300

    Update duration recording video on checkin page

--- src/locale/ar/user/checkin.js
+++ src/locale/ar/user/checkin.js
@@ -4,4 +4,10 @@ export default {
   checkout: "Checkout",
   scan_in_progress: "Facial scanning in progress...",
   blink_eyes: "Blink your eyes",
+  outside: {
+    title: "Outside Site Location",
+    description: "You are currently outside of your site location",
+    back: "Back",
+    try_again: "Try Again",
+  },
 };
