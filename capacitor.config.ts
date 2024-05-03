542cf26e3875ab2e80f2062e970200c45ae75f30
Ihor Bohdanov <bbengamin@gmail.com>
2024-05-03 18:21:16 +0300

    Change App ID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "io.ionic.onefm",
+  appId: "com.onefacilitiesmanagement.ios.apps",
   appName: "onefacilitiesmanagement",
   webDir: "dist",
   server: {
