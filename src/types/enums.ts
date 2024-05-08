768c5ecf18854da3eaf0276dc6f603e00c5b6f78
Ihor Bohdanov <ihor.bohdanov@right.link>
2024-05-08 16:26:13 +0300

    Add status and type enums

---
+++ src/types/enums.ts
@@ -0,0 +1,13 @@
+export enum LEAVE_TYPE {
+  SICK= "Sick leave",
+  MATERNITY= "Maternity Leave",
+  HAJJ= "Hajj Leave",
+  ANNUAL= "Annual Leave",
+}
+
+export enum LEAVE_STATUS {
+  OPENED = "Opened",
+  APPROVED = "Approved",
+  REJECTED = "Rejected",
+  CANCELLED = "Cancelled",
+}
