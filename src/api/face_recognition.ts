09fb5670259adb27f76c2e7c26d7270584899976
Ihor Bohdanov <ihor.bohdanov@right.link>
2024-06-06 11:37:26 +0300

    Update Content-Type for enrollment and checkin post video request

--- src/api/face_recognition.ts
+++ src/api/face_recognition.ts
@@ -6,6 +6,11 @@ type EnrollParams = {
 };
 
 export const enroll = async (data: EnrollParams) =>
-  await http.post(`v1.face_recognition.enroll`, { data });
+  await http.post(`v1.face_recognition.enroll`, {
+    data,
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 
 export default { enroll };
