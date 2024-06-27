73efd8f7f2e009dd069c1ff933536b65166b4cb4
Ihor Bohdanov <ihor.bohdanov@right.link>
2024-06-27 19:06:32 +0300

    Update default value of user id on the profile page

--- src/views/user/ProfilePage.vue
+++ src/views/user/ProfilePage.vue
@@ -40,7 +40,7 @@ const { formatImageUrl } = useDisplayImage();
 
 const user = reactive({
   name: "",
-  id: "",
+  id: userStore.user?.employee_id || "",
   picture: "",
   email: "",
   phone: "",
@@ -127,7 +127,6 @@ const getUserDetails = async () => {
     });
 
     user.name = data.message.Name;
-    user.id = data.message.EMP_ID;
     user.picture = data.message.User_Image;
     user.email = data.message.Email;
     user.phone = data.message.Mobile_no;
